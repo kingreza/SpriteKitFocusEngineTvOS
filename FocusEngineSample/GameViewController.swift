a39cb47ff093c765f0ce63dd05f9e5125f5d4fd6
Reza Shirazian <reza.shirazian@gmail.com>
2017-04-10 17:11:23 -0700

    Initial Commit

---
+++ FocusEngineSample/GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  FocusEngineSample
+//
+//  Created by Reza Shirazian on 2017-04-10.
+//  Copyright © 2017 Reza Shirazian. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
